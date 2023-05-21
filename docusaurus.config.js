e7da0f0ceb61eff7decce9c4b03b151538820f87
lb1001 <luobiao90@gmail.com>
2023-05-21 12:38:25 +0800

    change layout

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,20 +6,20 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Lb1001 Tech Book',
-  tagline: 'A Handy Knowledge Base',
-  favicon: 'img/favicon.ico',
+  title: 'Lb1001的知识手册',
+  tagline: '一个集混乱、随意及优雅于一身的大杂烩......',
+  favicon: 'img/siyecao.ico',
 
   // Set the production url of your site here
-  url: 'http://www.lb1001.tech/',
+  url: 'https://www.lb1001.tech/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'lb1001', // Usually your GitHub org/user name.
+  projectName: 'lb1001_tech_portal', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -62,21 +62,33 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      // image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: '首页',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/siyecao.svg',
         },
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
+            sidebarId: 'engineeringSidebar',
+            position: 'right',
+            label: '工程文档',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            type: 'docSidebar',
+            sidebarId: 'electricStuffSidebar',
+            position: 'right',
+            label: '电子小制作'
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'algoStuffSidebar',
+            position: 'right',
+            label: '算法小知识',
+          },
+          {to: '/blog', label: '速记', position: 'right'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
@@ -127,7 +139,7 @@ const config = {
         //     ],
         //   },
         // ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} lb1001 Tech Book. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
